403346b78c295ed6837dd47afbfc31b7715dcde8
Nehemiah <neheabe@gmail.com>
2025-05-16 12:51:46 -0400

    Just added my player fuunction that allows player 1 and player 2 to move the paddles, up and down, without exceeding barriers.

--- PingPongDesign.js
+++ PingPongDesign.js
@@ -25,14 +25,14 @@ const ball = {
     y: canvas.height / 2 - 5,    // I sub by 5 so that the ball is aligned and shifted to the center.
     width: 10,
     height: 10,
-    color: 'white'
+    color: 'white',
     dx: 3,   // This is the speed of the ball in the x direction (horizontally)
     dy: 3    // This is the speed of the ball in the y direction (Vertically)
 };
 
     //This essentially is going to keep the score tracking for the game, this is some basic stuff of the game.
     let player1Score = 0;
-    let player2Socer = 0;  
+    let player2Score = 0;  
 
 
     //This is going to track key presses
@@ -64,17 +64,54 @@ function drawGame() {
     drawPaddle(player1);
     drawPaddle(player2);         //Here I am calling the function of the 2 players and the ball.
     drawBall();
-}
+
 
 
 // Here I basically draw the scores for the game
     ctx.fillStyle = "white"; // changing the font color to white
     ctx.font = "20px Arial"; // Defining the font size and size
-    ctx.fillText(player1Score, 50, 30); // This just draws player 1's score at position (50,30)
+    ctx.fillText(player1Score, 50, 30); // This just draws player 1's score at position (50,30), near the the top.
     ctx.fillText(player2Score, canvas.width - 70, 30); // draws the second player score top the right side of the screen.
 
 
+ //Here this is the importnat part of the game, I am creating a function to move the paddles bassed off of keys that have been pressed.
 
-   
+
+
+//this just moves player 2's paddle up when the Arrow key is presseed, this piece of code also makes sure the paddle doesn;t go beyond the top barrier. This will be the same for Arrow Down as well.
+
+function movePaddles() {
+    if(keys['ArrowUp'] && player2.y > 0){
+    	player2.y -= player2.speed;              //move player up 
+     }
+
+// moves player 2 down when Arrow donw key is pressed, and makes sure it doesn't go beyond the botton.
+    if (keys['ArrowDown'] && player2.y < canvas.height - player2.height){
+        player2.y += player2.speed;                     //moves player down
+    }
+ 
+   // when w key is pressed moves player1 paddle up
+    if (keys['w'] && player1.y > 0) {
+        player1.y -= player1.speed; // Move paddle up by its speed value
+    }
+    
+   // moves player 1 down, when s key is pressed.
+    if (keys['s'] && player1.y < canvas.height - player1.height) {
+        player1.y += player1.speed; // Move paddle down by its speed value
+    }
+
+
+    }
+
+
+
+function moveBall(){
+     ball.x += ball.dx;
+     ball.y += ball.dy;
+
+
+
+}
+
+} 
 drawGame();              // Here I am calling the function.
-
